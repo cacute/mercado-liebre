6659e08691c1fac6be417d394e588b8ee6b1d449
Juan Cruz Botet <botetjuancruz@gmail.com>
2022-10-19 18:10:30 -0300

    arreglando html

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const path = require('path');
 var express = require('express');
 var app = express();
 var port = process.env.PORT||3000;
-app.use(express.static('public'))
-// respond with "hello world" when a GET request is made to the homepage
+app.use(express.static(path.resolve(__dirname, './public')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/views/home.html'));
 });
