1c1442b99385e03ea25e673f6faef8fa2b14b83e
Juan Cruz Botet <botetjuancruz@gmail.com>
2022-10-18 22:15:49 -0300

    commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const path = require('path');
 var express = require('express');
 var app = express();
 var port = process.env.PORT||3000;
+app.use(express.static('public'))
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hola');
+  res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 app.listen(port,function(){
     console.log("server on port 3000")
