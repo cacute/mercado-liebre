cf705c97d07252acef1d2aa506274152d508dae1
Juan Cruz Botet <botetjuancruz@gmail.com>
2022-10-19 18:19:42 -0300

    volviendo a arreglar html

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 var express = require('express');
 var app = express();
 var port = process.env.PORT||3000;
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, '/public')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/views/home.html'));
 });
