8af8d53874fbdfb2cc5b06d1a20b80f6c29a5c2f
Juan Cruz Botet <botetjuancruz@gmail.com>
2022-10-19 21:00:57 -0300

    arreglando

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/views/home.html'));
 });
-app.listen(port,function(){
-    console.log("server on port 3000")
-})
+const puerto = process.env.PORT || 3001;
+app.listen (puerto, () =>{
+    console.log ('Servidor corriendo en el puerto')
+});
